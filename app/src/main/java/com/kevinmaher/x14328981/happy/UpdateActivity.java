4de23cfa7daff0d31825b3bce939dc070237ce63
Kevin Maher <kevm66@gmail.com>
2018-07-05 20:39:36 +0100

    text field remains hidden until mood is selected on 'how are you feeling?' screen, submit button remains hidden until text text field is tapped

--- app/src/main/java/com/kevinmaher/x14328981/happy/UpdateActivity.java
+++ app/src/main/java/com/kevinmaher/x14328981/happy/UpdateActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -26,28 +28,72 @@ public class UpdateActivity extends AppCompatActivity {
         final Button btnHappy = (Button) findViewById(R.id.btn_update_happy);
         final Button btnIndifferent = (Button) findViewById(R.id.btn_update_indifferent);
         final Button btnSad = (Button) findViewById(R.id.btn_update_sad);
-        Button btnRecord = (Button) findViewById(R.id.btn_record);
+        final Button btnUpdate = (Button) findViewById(R.id.btn_update_update);
+        final TextView textViewUpdate = (TextView) findViewById(R.id.textView_update_update);
+        final String mood;
 
-        //input field
+        //make input box and submit button invisible to provide minimal experience
+        btnUpdate.setVisibility(View.INVISIBLE);
+        textViewUpdate.setVisibility(View.INVISIBLE);
+
+        btnHappy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnHappy.setBackgroundResource(R.drawable.ic_happy_green);
+                btnIndifferent.setBackgroundResource(R.drawable.ic_indifferent_grey);
+                btnSad.setBackgroundResource(R.drawable.ic_sad_grey);
+
+                textViewUpdate.setVisibility(View.VISIBLE);
+                textViewUpdate.setHint("What made you feel happy?");
+            }
+        });
+
+        btnIndifferent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnHappy.setBackgroundResource(R.drawable.ic_happy_grey);
+                btnIndifferent.setBackgroundResource(R.drawable.ic_indifferent);
+                btnSad.setBackgroundResource(R.drawable.ic_sad_grey);
+
+                textViewUpdate.setVisibility(View.VISIBLE);
+                textViewUpdate.setHint("What made you feel indifferent?");
+            }
+        });
+
 
         btnSad.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 btnHappy.setBackgroundResource(R.drawable.ic_happy_grey);
                 btnIndifferent.setBackgroundResource(R.drawable.ic_indifferent_grey);
+                btnSad.setBackgroundResource(R.drawable.ic_sad);
+
+                textViewUpdate.setVisibility(View.VISIBLE);
+                textViewUpdate.setHint("What made you feel sad?");
 
                 //                startActivity(new Intent(UpdateActivity.this, NavActivity.class));
             }
         });
 
-        btnRecord.setOnClickListener(new View.OnClickListener() {
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//                mood = textViewUpdate.getText();
+//                Toast.makeText(getActivity(), "Testing button 3", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(UpdateActivity.this, NavActivity.class));
+            }
+        });
+
+
+        textViewUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnUpdate.setVisibility(View.VISIBLE);
             }
         });
     }
 }
+
 
 //method 1
 //        btnHappy.setVisibility(View.INVISIBLE);
